6b4c57c3a2b4e1817a4693a6a770b3beafcf93ca
iamalfonso <al_marquezx@hotmail.com>
2017-04-26 19:08:59 +1000

    no  migrate no sqllite

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module Efc
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.log_level = :fatal
   end
 end
